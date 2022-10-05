c238fcf5527ac0a169ca56371ed1a93dac6e9c36
sasha <sasha.atalyan@gmail.com>
2022-10-05 21:48:19 +0300

    Initial commit

---
+++ src/main/java/ru/itmo/potatocoder228/lab2/web_lab2/HelloResource.java
@@ -0,0 +1,14 @@
+package ru.itmo.potatocoder228.lab2.web_lab2;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
